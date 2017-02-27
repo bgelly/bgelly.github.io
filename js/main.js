b6d95693581ee4178328b254e83079a41ec65d84
Sam Wheeler <swheeler111@gmail.com>
2017-02-26 22:15:21 -0500

    added accordion menu and styling;

--- js/main.js
+++ js/main.js
@@ -11,7 +11,17 @@ $(document).ready(() => {
     $('#advertiserinfo').show();
   });
 
-  $('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').focus()
-})
+  $('#myModal').on('shown.bs.modal', function() {
+    $('#myInput').focus()
+  })
+
+  function toggleIcon(e) {
+    $(e.target)
+      .prev('.panel-heading')
+      .find(".more-less")
+      .toggleClass('glyphicon-plus glyphicon-minus');
+  }
+
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
 });
