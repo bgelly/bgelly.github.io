a8c5731cdc13dd1844032f0c1046ba545e11386e
Sam Wheeler <swheeler111@gmail.com>
2017-04-23 15:25:32 -0400

    major changes to image assets and js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(() => {
+
+  $('#siteNav').affix({
+    offset: {
+      top: 100
+    }
+  })
+
   $('.driverinfo').hide();
 
   $('#showdriver').click(() => {
@@ -49,41 +56,41 @@ $(document).ready(() => {
 
   $('#driverbox1').mouseenter(() => {
     $('#driveicon1, #drivetitle1').css('color', '#7CA689');
-    $('#driverbox1').animate({ 'marginTop' : '+=5px' });
+    $('#driverbox1').animate({ 'marginTop': '+=5px' });
   });
 
   $('#driverbox1').mouseleave(() => {
     $('#driveicon1, #drivetitle1').css('color', '#221e1f');
-    $('#driverbox1').animate({ 'marginTop' : '-=5px' });
+    $('#driverbox1').animate({ 'marginTop': '-=5px' });
   });
 
   $('#driverbox2').mouseenter(() => {
     $('#driveicon2, #drivetitle2').css('color', '#F2D585');
-    $('#driverbox2').animate({ 'marginTop' : '+=5px' });
+    $('#driverbox2').animate({ 'marginTop': '+=5px' });
   });
 
   $('#driverbox2').mouseleave(() => {
     $('#driveicon2, #drivetitle2').css('color', '#221e1f');
-    $('#driverbox2').animate({ 'marginTop' : '-=5px' });
+    $('#driverbox2').animate({ 'marginTop': '-=5px' });
   });
 
   $('#driverbox3').mouseenter(() => {
     $('#driveicon3, #drivetitle3').css('color', '#D98661');
-    $('#driverbox3').animate({ 'marginTop' : '+=5px' });
+    $('#driverbox3').animate({ 'marginTop': '+=5px' });
   });
 
   $('#driverbox3').mouseleave(() => {
     $('#driveicon3, #drivetitle3').css('color', '#221e1f');
-    $('#driverbox3').animate({ 'marginTop' : '-=5px' });
+    $('#driverbox3').animate({ 'marginTop': '-=5px' });
   });
 
   $('#driverbox4').mouseenter(() => {
     $('#driveicon4, #drivetitle4').css('color', '#7CA689');
-    $('#driverbox4').animate({ 'marginTop' : '+=5px' });
+    $('#driverbox4').animate({ 'marginTop': '+=5px' });
   });
 
   $('#driverbox4').mouseleave(() => {
     $('#driveicon4, #drivetitle4').css('color', '#221e1f');
-    $('#driverbox4').animate({ 'marginTop' : '-=5px' });
+    $('#driverbox4').animate({ 'marginTop': '-=5px' });
   });
 });
