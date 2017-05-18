5d80b5e24bd93c893876e9a8367f52b43a825ff0
Sam Wheeler <swheeler111@gmail.com>
2017-05-18 15:55:21 -0400

    added basic firebase scripts

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCk93QI28nBqCzL0ioly4PZru_sN9A4dr4",
+    authDomain: "rideo-732af.firebaseapp.com",
+    databaseURL: "https://rideo-732af.firebaseio.com",
+    projectId: "rideo-732af",
+    storageBucket: "rideo-732af.appspot.com",
+    messagingSenderId: "431154143360"
+  };
+firebase.initializeApp(config);
+console.log('hello world')
