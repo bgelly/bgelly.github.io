975a57d8fa49a393612913fc2f987cff34a985a1
Sam Wheeler <swheeler111@gmail.com>
2017-05-18 18:15:38 -0400

    basic login set up

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,84 @@
 var config = {
-    apiKey: "AIzaSyCk93QI28nBqCzL0ioly4PZru_sN9A4dr4",
-    authDomain: "rideo-732af.firebaseapp.com",
-    databaseURL: "https://rideo-732af.firebaseio.com",
-    projectId: "rideo-732af",
-    storageBucket: "rideo-732af.appspot.com",
-    messagingSenderId: "431154143360"
-  };
+  apiKey: "AIzaSyCk93QI28nBqCzL0ioly4PZru_sN9A4dr4",
+  authDomain: "rideo-732af.firebaseapp.com",
+  databaseURL: "https://rideo-732af.firebaseio.com",
+  projectId: "rideo-732af",
+  storageBucket: "rideo-732af.appspot.com",
+  messagingSenderId: "431154143360"
+};
 firebase.initializeApp(config);
 
 
+// upon redirect, check if a user is logged in
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log('theres a user!')
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    console.log(user)
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
+
 $(document).ready(() => {
-  console.log('hello worldddddddd')
-  $('#loginbutton').click(function() {
-    console.log('hello world')
+  $('#loginbutton').on('click', function(e) {
+    e.preventDefault();
+    // $('#loginModal').modal('hide');
+    // $('#messageModalLabel').html('<span class="text-center text-info"><i class="fa fa-cog fa-spin"></i></span>');
+    // $('#messageModal').modal('show');
+
+    if ($('#loginEmail').val() != '' && $('#loginPassword').val() != '') {
+      console.log('in if')
+      var userEmail = $('#loginEmail').val();
+      var userPassword = $('#loginPassword').val();
+
+      firebase.auth().signInWithEmailAndPassword(userEmail, userPassword)
+      .then(function(data) {
+        // successful login, redirect to login page
+        window.location = "pages/data.html"
+      })
+      .catch(function(error) {
+        // error handling
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log('Error: ', errorCode, " - ", errorMessage)
+      });
+
+
+      // dbRef.authWithPassword({
+      //   email: $('#loginEmail').val(),
+      //   password: $('#loginPassword').val()
+      // }, function(error, authData) {
+      //   if (error) {
+      //     console.log("Login Failed!", error);
+      //     $('#messageModalLabel').html('<span class="text-danger">ERROR: ' + error.code + '</span>')
+      //   } else {
+      //     console.log("Authenticated successfully with payload:", authData);
+      //     auth = authData;
+      //     $('#messageModalLabel').html('<span class="text-center text-success">Success!</span>')
+      //     setTimeout(function() {
+      //       $('#messageModal').modal('hide');
+      //       $('.unauthenticated, .userAuth').toggleClass('unauthenticated').toggleClass('authenticated');
+      //       contactsRef
+      //         .child(auth.uid)
+      //         .on("child_added", function(snap) {
+      //           console.log("added", snap.key(), snap.val());
+      //           $('#contacts').append(contactHtmlFromObject(snap.val()));
+      //         });
+      //     })
+      //   }
+      // });
+    } else {
+      console.log('nope')
+    }
   })
 })
